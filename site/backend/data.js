7f9dbbd3f742597f9b2a4c0aab1ea5639004275e
Diogo Reis <diogommreis1@gmail.com>
2020-04-12 00:34:58 +0100

    Saving data to the about page in js file

--- site/backend/data.js
+++ site/backend/data.js
@@ -3,5 +3,5 @@ module.exports = [
       title: "Backend em Node.js",
       img: "nodejslogo.png",
       info: "Domino basicamente a linguagem javascript e já possuo conhecimentos em node.js"
-   },
+   }
 ];
